913b22dd07fcaa6956c123216eac352991d1306a
mishuk09 <mishukinfo09@gmail.com>
2025-03-24 05:46:48 +0600

    update post component

--- Schema/Post.js
+++ Schema/Post.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose');
 
 const itemSchema = new mongoose.Schema({
-    img: { type: String, required: true },  // Encoded string
+    img: { type: [String], required: true },  // Store multiple image paths
     category: { type: String, required: true },
     title: { type: String, required: true },
     newPrice: { type: Number, required: true },
     oldPrice: { type: Number, required: true },
-    color: { type: [String], required: false },  // Array of strings
-    size: { type: [String], required: false },   // Array of strings
-    description: { type: String, required: false },  // Corrected typo
+    stock: { type: Number, required: true },
+    color: { type: [String], required: false },
+    size: { type: [String], required: false },
+    description: { type: String, required: false },
 });
 
 const Post = mongoose.model('Post', itemSchema);
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 require('dotenv').config();
 const app = express();
 const cors = require('cors')
-const port = 5000;
+const port = 5001
 const mongoose = require('mongoose');
 const auth = require('./modules/auth.js');
 const items = require('./modules/items');
